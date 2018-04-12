b14513e97bad6a25cfb9a6fcc922a0583726307d
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.com>
2018-04-12 15:49:47 +0200

    Move ProfileButton out of SettingsMenuScene

---
+++ YouWillExplode/YouWillExplode/Profiles/ProfileButton.cs
@@ -0,0 +1,32 @@
+﻿namespace YouWillExplode
+{
+    using DatabaseContract;
+    using Microsoft.Xna.Framework;
+
+    internal sealed class ProfileButton : Entity
+    {
+        private Profile profile;
+
+        public ProfileButton(Profile profile, ProfileEditor editor, SettingsMenuScene scene, Button.Resources resources)
+        {
+            this.Button =
+                new RemoveableButton(
+                    new Button(
+                        new Vector2(92, 32),
+                        profile.Name,
+                        () => editor.EditProfile(this.profile),
+                        resources),
+                    () => scene.Destroy(this),
+                    resources)
+                {
+                    Spacing = 4
+                };
+
+            scene.Manage(this.Button);
+
+            this.profile = profile;
+        }
+
+        public RemoveableButton Button { get; private set; }
+    }
+}
--- YouWillExplode/YouWillExplode/Scenes/SettingsMenuScene.cs
+++ YouWillExplode/YouWillExplode/Scenes/SettingsMenuScene.cs
@@ -85,32 +85,5 @@
         private void RemoveProfile(Profile profile)
         {
         }
-
-        private class ProfileButton : Entity
-        {
-            private Profile profile;
-
-            public ProfileButton(Profile profile, ProfileEditor editor, SettingsMenuScene scene, Button.Resources resources)
-            {
-                this.Button =
-                    new RemoveableButton(
-                        new Button(
-                            new Vector2(92, 32),
-                            profile.Name,
-                            () => editor.EditProfile(this.profile),
-                            resources),
-                        () => scene.Destroy(this),
-                        resources)
-                    {
-                        Spacing = 4
-                    };
-
-                scene.Manage(this.Button);
-
-                this.profile = profile;
-            }
-
-            public RemoveableButton Button { get; private set; }
-        }
     }
 }
